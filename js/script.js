be8b54d98c41fe3a5125e1704a3b9f39675341b5
ImLuze <luzedz@gmail.com>
2018-08-13 11:41:41 +0200

    Added random rotation to asteroids

--- js/script.js
+++ js/script.js
@@ -7,19 +7,20 @@
   class AsteroidBelt {
 		constructor(options) {
 			this.mesh = new THREE.Object3D();
-			this.nAsteroids = 1;
+			this.nAsteroids = 5;
 
 			for(let i=0; i<this.nAsteroids; i++) {
 				const c = new Asteroid();
 
-				c.mesh.position.x = 0;
-				c.mesh.position.y = 0;
+				c.mesh.position.x = Math.random() * 300;
+				c.mesh.position.y = Math.random() * 300;
 				c.mesh.position.z = -400;
 
 				const s = 1+Math.random()*2;
 				c.mesh.scale.set(s, s, s);
 
 				this.mesh.add(c.mesh);
+        this[i] = c.rotationValue;
 			}
 		}
 	}
@@ -27,6 +28,8 @@
   class Asteroid {
 		constructor(options) {
 			this.mesh = new THREE.Object3D();
+      this.rotationValue = (Math.random() * .03);
+      this.rotationValue *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
 
 			const geom = new THREE.BoxGeometry(20, 20, 20);
 
@@ -133,11 +136,18 @@
   const createAsteroidBelt = () => {
     asteroidBelt = new AsteroidBelt();
     scene.add(asteroidBelt.mesh);
-    console.log(scene);
+  }
+
+  const isOffScreen = object => {
+    console.log(object);
+
   }
 
   const loop = () => {
-    asteroidBelt.mesh.rotation.z += .01;
+    for(let i = 0; i < asteroidBelt.mesh.children.length; i++) {
+      asteroidBelt.mesh.children[i].rotation.z += asteroidBelt[i];
+      isOffScreen(asteroidBelt.mesh.children[i]);
+    }
 
     renderer.render(scene, camera);
     requestAnimationFrame(loop);
